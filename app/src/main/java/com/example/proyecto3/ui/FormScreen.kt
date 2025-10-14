cdb909d1f094bc79f05e4068b91ed0245d16ef79
javier <ja.quirogab@duocuc.cl>
2025-10-14 14:09:13 -0300

    commit2

--- app/src/main/java/com/example/proyecto3/ui/FormScreen.kt
+++ app/src/main/java/com/example/proyecto3/ui/FormScreen.kt
@@ -2,11 +2,14 @@ package com.example.proyecto3.ui
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.example.proyecto3.viewmodel.ExpenseViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormScreen(
-    vm:ExpenseViewModel,
+    vm: ExpenseViewModel,
     onBack:()-> Unit,
     onSaved:()-> Unit
 ){
